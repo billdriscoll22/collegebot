c32eec2aadc9d22af066a657a62ed1f1a62d8d40
Bill Driscoll <bill.driscoll22@gmail.com>
2016-02-25 15:32:30 -0800

    demo program

---
+++ college_bot.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv('spreadsheet.csv', index_col = 0)
+X = data[['gpa', 'gender', 'first_gen', 'is_urm', 'sat']]
+y = data['status']
+log_reg_engine = LogisticRegression()
+log_reg_engine.fit(X, y)
+#test = [4.0, 1, 1, 1, 2400]
+#print log_reg_engine.predict([test])
+print "Welcome to College Bot."
+gpa = raw_input("enter gpa ")
+gender = raw_input("enter gender. 0 is male, 1 is female ")
+first_gen = raw_input("is the student first gen? 1 if yes, 0 if no ")
+urm =  raw_input("is the student urm? 1 if yes, 0 if no ")
+sat = raw_input("enter sat ")
+test_array = [float(gpa), float(gender), float(first_gen), float(urm), float(sat)]
+prediction = log_reg_engine.predict([test_array])
+prediction = prediction[0]
+if prediction == 0:
+  print "rejected"
+elif prediction == 1:
+  print "deferred"
+else:
+  print "accepted"
