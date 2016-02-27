9f8a6f084c5859074459612fc2df2b1881f28bf1
Bill Driscoll <bill.driscoll22@gmail.com>
2016-02-27 11:27:10 -0800

    Added null value protection using imputer

--- college_bot.py
+++ college_bot.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import Imputer
 
 data = pd.read_csv('spreadsheet.csv', index_col = 0)
 X = data[['gpa', 'gender', 'first_gen', 'is_urm', 'sat']]
 y = data['status']
+imp = Imputer()
+X = imp.fit_transform(X)
 log_reg_engine = LogisticRegression()
 log_reg_engine.fit(X, y)
 #test = [4.0, 1, 1, 1, 2400]
 #print log_reg_engine.predict([test])
-print "Welcome to College Bot."
+print "Welcome to College Bot, now with null value protection!"
 gpa = raw_input("enter gpa ")
 gender = raw_input("enter gender. 0 is male, 1 is female ")
 first_gen = raw_input("is the student first gen? 1 if yes, 0 if no ")
